ea191aea8182cf4f2c01af0fe386938d371b7722
Ridian12 <manescum04@gmail.com>
2025-05-23 22:36:56 +0300

    Update server.py

--- server.py
+++ server.py
@@ -106,6 +106,7 @@ def login():
 def create_tables():
     db.create_all()
 
-# ---------------------- MAIN ----------------------
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=5000)
